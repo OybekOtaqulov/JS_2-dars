59bf842a3c60de77a132d7ea36f52eed1a8fdf59
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-20 12:33:41 +0500

    JS 2-dars

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/*let a = 44;
+
+if (a == 45){
+    console.log("a qiymat 44dan katta yoki teng!")
+}else{
+    console.log("a ning qiymati ko'rsatilgan sondan katta yoki teng emas!")
+}*/
+
+let a = 10;
+let b = 46;
+let c = 50;
+let d = 55;
+
+/*if(a == 10 && b == 46 && c == 50 && d == 55){
+    console.log("Shartlarni hammasi bajarildi")
+}
+else{
+    console.log("Shartlarning kamida bittasi ishlamayapti ")
+}*/
+
+/*if (a == 11 || b == 48 || c == 53 || d == 50){
+    console.log("Shartlrning kamida bittasi ishlayapti")
+}
+else{
+    console.log("Shartlarni hammasi bajarilmadi")
+}*/
+
+/*JS data types: undefined,boolean,object*/
+
+let soz = "text";
+let son = 45;
+let math;
+/*let rost = 45==45?true:false;*/
+
+/*if (45 == 44){
+    document.write("True");
+}else{
+    document.write("False");
+}*/
+
+/*let arrayObject = [45,78,"text","matn",true,false];
+let obbiyObject = {ismi:"Emili", familiyasi: "Gluhova", yoshi:15, manzil: "Sankt-Peterburg"};
+let talabami = "Ha, talaba"
+if (obbiyObject.yoshi < 19){
+    talabami = "Yoq, talaba emas"
+}
+
+console.log(talabami);*/
+
+let obbiyObject = {ismi:"Garri Potter kitobi", yaxshimi: "60"};
+let yaxshimi = "                -Ha, bu zo'r kitob"
+if (obbiyObject.yaxshimi > 70){
+    yaxshimi = "                -Yoq, bu yomon kitob"
+}
+
+console.log("-'Garri Potter' yaxshi kitobmi? " + yaxshimi);
+
+
+
